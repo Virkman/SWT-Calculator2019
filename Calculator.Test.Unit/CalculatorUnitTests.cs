b1875bf14c8614f3a05fa85d113e121d9aa4604c
Virkman <nvirkman@gmail.com>
2019-08-26 17:01:54 +0200

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    class CalculatorUnitTests
+    {
+        private Calculator _calc;
+
+        [SetUp]
+        public void setup()
+        {
+            _calc = new Calculator();
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(3, 5, 8)]
+        [TestCase(7, 8, 15)]
+        [TestCase(10, 50, 60)]
+        [TestCase(12, 76, 88)]
+        public void CalculatorAdd_ReturnTrue(double a, double b, double sum)
+        {
+            Assert.That(_calc.Add(a, b), Is.EqualTo(sum));
+        }
+
+        [TestCase(2, 1, 1)]
+        [TestCase(5, 3, 2)]
+        [TestCase(8, 7, 1)]
+        [TestCase(50, 10, 40)]
+        [TestCase(76, 12, 64)]
+        public void CalculatorSubtract_ReturnTrue(double a, double b, double sum)
+        {
+            Assert.That(_calc.Subtract(a, b), Is.EqualTo(sum));
+        }
+
+        [TestCase(2, 1, 2)]
+        [TestCase(5, 3, 15)]
+        [TestCase(8, 7, 56)]
+        [TestCase(50, 10, 500)]
+        [TestCase(76, 12, 912)]
+        public void CalculatorMultiply_ReturnTrue(double a, double b, double sum)
+        {
+            Assert.That(_calc.Multiply(a, b), Is.EqualTo(sum));
+        }
+
+        [TestCase(2, 1, 2)]
+        [TestCase(5, 3, 125)]
+        [TestCase(8, 7, 2097152)]
+        [TestCase(50, 3, 125000)]
+        [TestCase(5, 2, 25)]
+        public void CalculatorPower_ReturnTrue(double a, double b, double sum)
+        {
+            Assert.That(_calc.Power(a, b), Is.EqualTo(sum));
+        }
+    }
+}
---
+++ SWT-Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_Calculator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
